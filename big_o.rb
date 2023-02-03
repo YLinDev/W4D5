8cb6d747c4eb299b68022e2be73f7ddf29b9c310
Sean Abbas <sean.abbas33@gmail.com>
2023-02-03 13:59:20 -0500

    subsum

--- big_o.rb
+++ big_o.rb
@@ -18,4 +18,22 @@ def my_min2(list) #O(n^2)
     end
 end
 
-p my_min2(list)  # =>  -5
+# p my_min2(list)  # =>  -5
+
+def subsum(list)
+    max = list[0]
+    (0...list.length).each do |i|
+        # if max < list[0..i].sum
+        #     max = list[0..i].sum
+        # end
+        (i...list.length).each do |j|
+            if max < list[i..j].sum
+                max = list[i..j].sum
+            end
+        end
+    end
+    max
+end
+
+list = [-2, -1, -6, -7, -6, -7]
+p subsum(list) # => 8 (from [7, -6, 7])
